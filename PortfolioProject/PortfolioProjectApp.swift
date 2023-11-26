6e5f0907f1ef527ddaf99d94653f91131e0b4ae8
Xursandbek <xursandbek06082001@mail.com>
2023-11-26 16:58:23 +0500

    Initial Commit

---
+++ PortfolioProject/PortfolioProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioProjectApp.swift
+//  PortfolioProject
+//
+//  Created by Xursandbek Qambaraliyev on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
